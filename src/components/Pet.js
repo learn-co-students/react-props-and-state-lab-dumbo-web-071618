e2f736a01a5c80202467850c9c93d4465f903139
joalorro <joalorro@gmail.com>
2018-09-20 11:53:46 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,47 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
+  handleClick = (event) => {
+    this.props.onAdoptPet(event.target.dataset.id)
+  }
+
+  // renderButton = () => {
+  //   if (this.props.pet.isAdpoted) {
+  //     return <button className="ui disabled button">Already adopted</button>
+  //   } else {
+  //     return <button onClick={this.handleClick} className="ui primary button" data-id={this.props.pet.id}>Adopt pet</button>
+  //   }
+  // }
+
+  renderGender = () => {
+    if (this.props.pet.gender === "male") {
+      return "♂"
+    } else {
+      return "♀"
+    }
+  }
   render() {
+    const onAdoptPet = this.props.onAdoptPet
+    let id = this.props.pet.id
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.renderGender()}
+            {this.props.pet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {/* {this.renderButton()} */}
+          {this.props.pet.isAdopted ? <button className="ui disabled button">Already adopted</button> : <button onClick={ () => onAdoptPet(id)} className="ui primary button" data-id={this.props.pet.id}>Adopt pet</button> }
         </div>
       </div>
     )
