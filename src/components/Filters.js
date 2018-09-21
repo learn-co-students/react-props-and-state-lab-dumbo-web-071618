564156da4f0b6ebc07f09091069936bd36ef5285
Gabriel-Batista <gabedbatista@gmail.com>
2018-09-21 09:39:27 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,25 +1,46 @@
 import React from 'react'
 
 class Filters extends React.Component {
-  render() {
-    return (
-      <div className="ui form">
-        <h3>Animal type</h3>
-        <div className="field">
-          <select name="type" id="type">
-            <option value="all">All</option>
-            <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
-          </select>
-        </div>
+    constructor(props)  {
+        super(props)
+        this.state = {
+            selectedValue: "all"
+        }
+    }
+    
+    handleChange = (event) => {
+        this.setState({
+            selectedValue: event.target.value
+        })
+    }
 
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="ui form" >
+                <h3>Animal type</h3>
+                <div className="field">
+                    <select name="type" id="type" value={this.state.selectedValue} onChange={this.handleChange}>
+                        <option value="all">All</option>
+                        <option value="cat">Cats</option>
+                        <option value="dog">Dogs</option>
+                        <option value="micropig">Micropigs</option>
+                    </select>
+                </div>
+
+                <div className="field">
+                    <button 
+                    className="ui secondary button" 
+                    onClick={() => {
+                        this.props.onChangeType(this.state.selectedValue)
+                        .then (this.props.onFindPetsClick)
+                        }}
+                    >
+                    Find pets
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Filters
